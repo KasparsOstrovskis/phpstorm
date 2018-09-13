447f2bdfaab1e1200b1e22b487063f02476545fa
Kaspars Ostrovskis <o.kaspars1991@gmail.com>
2018-09-13 11:47:58 +0300

    added 11

--- functions.js
+++ functions.js
@@ -1,9 +1,12 @@
 function numberInWords(number) {
     var ret;
-    var ones =["one","two","threes","four","five","six","seven","eight","nine","ten"];
+    var ones =["one","two","three","four","five","six","seven","eight","nine","ten"];
     if (number>=1||number <=10){
 
         ret=ones[parseInt(number)-1];
+    }
+    if (parseInt(number)==11){
+        ret = "eleven";
     }
     return ret;
 }
