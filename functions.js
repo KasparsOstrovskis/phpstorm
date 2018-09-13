af6edf824008cb5379f93ab0c2a4e8abdf1fa229
Kaspars Ostrovskis <o.kaspars1991@gmail.com>
2018-09-13 11:36:01 +0300

    First commit

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 function numberInWords(number) {
     var ret;
-    var ones =["one","two","three","four","five","six","seven","eight","nine","ten"];
+    var ones =["one","two","threes","four","five","six","seven","eight","nine","ten"];
     if (number>=1||number <=10){
 
         ret=ones[parseInt(number)-1];
